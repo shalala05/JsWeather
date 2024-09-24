2aac8139d5af04164ded3d35fe6b22e5339c295d
shalala05 allahverdiyeva <allahverdiyevashelale05+shalala05@gmail.com>
2024-09-24 19:36:19 +0000

    weather

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
                 $("#city").html(data.name);
                 $("#temp").html(data.main.temp);
                 $("#feels").html(data.main.feels_like);
-                $("myWeather").val("");
+                $("#myWeather").val("");
                 console.log(data);
             }
             myweather();
@@ -27,7 +27,7 @@ $(document).ready(function () {
             $("#city").html(data.name);
             $("#temp").html(data.main.temp);
             $("#feels").html(data.main.feels_like);
-            $("myWeather").val("");
+            $("#myWeather").val("");
             console.log(data);
         }
         myweather();
